c41950523d62931f524fa2d5b6c0e9c55d387bb9
HAFIZUR RAHMAN <hafizur.rahmanc@gmail.com>
2016-09-26 10:35:11 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,14 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients
+
+  # def ingredients_attributes=(ingredients_attributes)
+  #   ingredients_attributes.each do |i, ingredient_attributes|
+  #     self.ingredients.build(ingredient_attributes)
+  #   end
+  # end
+
+  # def ingredient_attributes
+  #   Recipe.ingredients.map(&:quantity)
+  # end
 end

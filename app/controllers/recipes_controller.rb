2e52c67f70ea09cdcb9751ebcd398df9161a9346
Esmery Corniel <esmerycornielle@gmail.com>
2016-10-25 10:26:45 -0400

    done

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,26 @@
+class RecipesController < ApplicationController
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+    @recipe.ingredients.build()
+    @recipe.ingredients.build()
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
+  end
+
+end
